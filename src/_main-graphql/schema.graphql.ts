ff33797767fbdf6f6432ea7eb14b60c9d88fbab9
SnapraiseInAction <Reginald.Osano.Contractor@snapraise.com>
2022-08-18 19:13:47 +0800

    Code cleanup

--- src/_main-graphql/schema.graphql.ts
+++ src/_main-graphql/schema.graphql.ts
@@ -1,5 +1,7 @@
 import { gql } from "apollo-server";
 
+import { AuthType } from "../authentication/graphql/auth-gql";
+
 import {
   UserType,
   UserCreate,
@@ -7,8 +9,6 @@ import {
   UpdateUser,
   DeleteUser,
 } from "../user/graphql/user-gql";
-
-import { AuthType } from "../authentication/graphql/auth-gql";
 
 import {
   StudentType,
